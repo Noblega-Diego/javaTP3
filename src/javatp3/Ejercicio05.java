d021d2ef4e15d1afad5c380f2fc26eed00e88281
diego <diego@laptop-swift3.com>
2020-09-24 13:23:11 -0300

    inicio ejercicio 5

---
+++ src/javatp3/Ejercicio05.java
@@ -0,0 +1,18 @@
+/**
+ * Escriba una aplicación para almacenar aleatoriamente 20 números enteros positivos pares
+ * del 1 al 100, de los cuales se desea saber:
+ * a) su promedio aritmético
+ * b) cuántos de los números son iguales al promedio aritmético
+ * c) cuántos de los números son mayores que el promedio aritmético
+ * d) cuántos de los números son menores que el promedio aritmético 
+ */
+package javatp3;
+/**
+ *
+ * @author diego
+ */
+public class Ejercicio05 {
+    public static void main(String[] args) {
+        
+    }
+}
